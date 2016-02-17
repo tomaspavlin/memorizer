0fe33affc3921ebc1eb32d5ccfc34f92d4ce43d7
tomas <tomaspavlin@notebook.com>
2016-02-17 13:17:32 +0100

    day_routine.sh bug fixed, data files deleted from git

--- day_routine.sh
+++ day_routine.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Starting day routine for day" $(date '+%d %b %Y, %a')
+cd `dirname $0`
+
 echo "Starting scheduler..."
 ./scheduler.py
 
 echo "Starting uploader..."
 ./uploader.py
+
 echo "==="
 echo ""
